4369f0b86700a293c639960ec70602780555da8f
robert611 <cyrkiel86@interia.pl>
2022-08-04 16:42:13 +0200

    Create login and register forms plus logout endpoint

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use JetBrains\PhpStorm\NoReturn;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class SecurityController extends AbstractController
+{
+    #[NoReturn] #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(TranslatorInterface $translator): void
+    {
+       exit($translator->trans('activate_logout_in_security'));
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    #[Assert\NotBlank]
+    #[Assert\Length(
+        min: 8,
+        max: 64,
+        minMessage: 'user.username.length.min',
+        maxMessage: 'user.username.length.max'
+    )]
+    private string $username;
+
+    #[ORM\Column(type: 'json')]
+    private array $roles = [];
+
+    #[ORM\Column(type: 'string')]
+    #[Assert\Length(
+        min: 8,
+        max: 64,
+        minMessage: 'user.password.length.min',
+        maxMessage: 'user.password.length.max'
+    )]
+    private string $password;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\Email]
+    private string $email;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    #[Assert\IsTrue(message: 'user.password_safe.is_true')]
+    public function isPasswordSafe(): bool
+    {
+        if (!isset($this->password)) {
+            return true;
+        }
+
+        return $this->getUsername() !== $this->getPassword();
+    }
+}
