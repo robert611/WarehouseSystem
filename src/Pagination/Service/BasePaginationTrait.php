7426c53432613bf3f2022e3f72a922b8285a2137
robert611 <cyrkiel86@interia.pl>
2022-12-22 20:58:46 +0100

    Fix logic in fundamental pagination functions of BasePaginationTrait

--- src/Pagination/Service/BasePaginationTrait.php
+++ src/Pagination/Service/BasePaginationTrait.php
@@ -4,22 +4,22 @@ namespace App\Pagination\Service;
 
 trait BasePaginationTrait
 {
-    public function getPreviousPage(int $pageNumber, int $pagesCount): int
+    public function getPreviousPage(int $pageNumber, int $pagesCount): ?int
     {
-        if ($pageNumber < 1 || $pageNumber >= $pagesCount) {
-            return 1;
+        if ($pageNumber < 2) {
+            return null;
         }
 
-        return $pageNumber;
+        return $pageNumber - 1;
     }
 
-    public function getNextPage(int $pageNumber, int $pagesCount): int
+    public function getNextPage(int $pageNumber, int $pagesCount): ?int
     {
-        if ($pageNumber > $pagesCount) {
-            return $pagesCount;
+        if ($pageNumber >= $pagesCount) {
+            return null;
         }
 
-        return $pageNumber;
+        return $pageNumber + 1;
     }
 
     public function getPagesCount(int $totalItems, int $resultsPerPage): int
